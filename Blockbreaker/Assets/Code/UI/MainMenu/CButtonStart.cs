95a4357e3ff5e41dde572d8695a5454dc124ad5b
Florian Dunz <X-TremeGamer@web.de>
2017-03-07 11:45:15 +0100

    added a main menu

---
+++ Blockbreaker/Assets/Code/UI/MainMenu/CButtonStart.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CButtonStart : MonoBehaviour {
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+}
