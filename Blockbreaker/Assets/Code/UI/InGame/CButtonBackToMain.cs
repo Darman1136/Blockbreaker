b12504da5c5568634ee57c4a7a2d9b956159216d
Florian Dunz <X-TremeGamer@web.de>
2017-04-25 19:25:49 +0200

    added a back to main menu button on game over screen

---
+++ Blockbreaker/Assets/Code/UI/InGame/CButtonBackToMain.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CButtonBackToMain : MonoBehaviour {
+    public void Back() {
+        SceneManager.LoadScene(0);
+    }
+}
