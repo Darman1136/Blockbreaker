fa94a0a91b610f0a91b68c3a6d1c774c3ec0b325
Florian Dunz <X-TremeGamer@web.de>
2017-04-30 23:05:30 +0200

    added a camera ratio fix to prevent game screen being cut off
    preparation of additional block types

---
+++ Blockbreaker/Assets/Code/Camera/CCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCamera : MonoBehaviour {
+
+    public float orthographicSize = 5;
+    public float aspect = 0f;
+    void Start() {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(
+                -orthographicSize * aspect, orthographicSize * aspect,
+                -orthographicSize, orthographicSize,
+                Camera.main.nearClipPlane, Camera.main.farClipPlane);
+    }
+}
